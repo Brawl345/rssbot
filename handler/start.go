a4813a4904614d2391f323193389441a3c68485d
Andreas Bielawski <brawl@nyanya.de>
2022-03-07 15:26:06 +0100

    Update help text

--- handler/start.go
+++ handler/start.go
@@ -8,9 +8,10 @@ import (
 func (h Handler) OnStart(c telebot.Context) error {
 	var sb strings.Builder
 	sb.WriteString("<b>/rss</b> <i>[Chat]</i>: Abonnierte Feeds anzeigen\n")
+	sb.WriteString("<b>/rss_all</b>: Alle abonnierten Feeds aus jedem Chat anzeigen\n")
 	sb.WriteString("<b>/sub</b> <i>Feed-URL</i> <i>[Chat]</i>: Feed abonnieren\n")
-	sb.WriteString("<b>/del</b> <i>Feed-URL</i> <i>[Chat]</i>: Feed löschen\n")
-	sb.WriteString("<i>[Chat]</i> ist ein optionales Argument mit dem @Kanalnamen.")
+	sb.WriteString("<b>/del</b> <i>Feed-URL</i> <i>[Chat]</i>: Feed deabonnieren\n\n")
+	sb.WriteString("<i>[Chat]</i> ist ein optionales Argument mit dem <code>@Kanalnamen</code>.")
 
 	return c.Send(sb.String(), defaultSendOptions)
 }

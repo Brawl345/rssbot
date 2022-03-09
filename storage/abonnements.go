e6a6933fa62cc58d283c40de4f4254922faa116c
Andreas Bielawski <brawl@nyanya.de>
2022-03-09 07:58:18 +0100

    Rename files for more sanity

--- storage/abonnements.go
+++ storage/abonnements.go
@@ -2,7 +2,9 @@ package storage
 
 import (
 	"context"
+	"database/sql"
 	"github.com/jmoiron/sqlx"
+	"github.com/mmcdole/gofeed"
 	"time"
 )
 
@@ -30,6 +32,14 @@ type (
 		ID        int64     `db:"chat_id"`
 		Title     string    `db:"title"`
 		CreatedAt time.Time `db:"chat_created_at"`
+	}
+
+	Feed struct {
+		ID        int64          `db:"id"`
+		Url       string         `db:"url"`
+		LastEntry sql.NullString `db:"last_entry"`
+		CreatedAt time.Time      `db:"created_at"`
+		UpdatedAt sql.NullTime   `db:"updated_at"`
 	}
 )
 
@@ -202,4 +212,22 @@ WHERE feeds.url = ?`
 
 	_, err := db.Exec(query, lastEntry, feedUrl)
 	return err
+}
+
+func (feedToCheck Feed) Check(lastEntry *string) (*gofeed.Feed, error) {
+	feed, err := gofeed.NewParser().ParseURL(feedToCheck.Url)
+	if err != nil {
+		return nil, err
+	}
+
+	if lastEntry != nil {
+		for i, item := range feed.Items {
+			if item.GUID == *lastEntry {
+				feed.Items = feed.Items[:i]
+				return feed, nil
+			}
+		}
+	}
+
+	return feed, nil
 }
